28e0e619679de5a6e39ba027b21047bafc6a8cd9
Powerekk <powerbird1234@gmail.com>
2023-04-26 22:10:41 +0200

    zadanko stg webapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Stg_zad_2.25';
+  citizen = "47,272";
+  geoCord = "53°57′47″N, 18°31′33″E";
+  powierzchnia = "25,28 km²";
 }
